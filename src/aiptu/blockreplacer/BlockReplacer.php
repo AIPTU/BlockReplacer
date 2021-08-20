17a5dafe8a519f79764a64efe02624293f0c7111
AIPTU <anangp808@gmail.com>
2021-08-20 14:00:42 +0700

    Config check on enable and formatting

--- src/aiptu/blockreplacer/BlockReplacer.php
+++ src/aiptu/blockreplacer/BlockReplacer.php
@@ -10,101 +10,96 @@ use pocketmine\block\Solid;
 use pocketmine\item\ItemFactory;
 use pocketmine\event\Listener;
 use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
 
-class BlockReplacer extends PluginBase implements Listener{
-    
-    public function onLoad() : void{
-        $this->checkConfig();
-    }
-    
-    public function onEnable() : void{
+class BlockReplacer extends PluginBase implements Listener {
+
+    public function onEnable() : void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->checkConfig();
         UpdateNotifier::checkUpdate($this->getDescription()->getName(), $this->getDescription()->getVersion());
     }
-    
-    private function checkConfig() : void{
+
+    private function checkConfig() : void {
         $this->saveDefaultConfig();
         ConfigUpdater::checkUpdate($this, $this->getConfig(), "config-version", 1);
-        if(!is_array($this->getConfig()->getAll()["blocks"])){
+        if (!is_array($this->getConfig()->getAll()["blocks"])) {
             $this->getLogger()->error("Config error: blocks must an array, disable plugin...");
             $this->getServer()->getPluginManager()->disablePlugin($this);
             return;
         }
-        if(!is_array($this->getConfig()->getAll()["worlds"])){
+        if (!is_array($this->getConfig()->getAll()["worlds"])) {
             $this->getLogger()->error("Config error: worlds must an array, disable plugin...");
             $this->getServer()->getPluginManager()->disablePlugin($this);
             return;
         }
-        if(!is_bool($this->getConfig()->get("auto-pickup", true))){
+        if (!is_bool($this->getConfig()->get("auto-pickup", true))) {
             $this->getLogger()->error("Config error: auto-pickup must an bool, disable plugin...");
             $this->getServer()->getPluginManager()->disablePlugin($this);
             return;
         }
-        if(!is_numeric($this->getConfig()->get("cooldown", 60))){
+        if (!is_numeric($this->getConfig()->get("cooldown", 60))) {
             $this->getLogger()->error("Config error: cooldown must an integer, disable plugin...");
             $this->getServer()->getPluginManager()->disablePlugin($this);
             return;
         }
-        foreach($this->getConfig()->getAll()["blocks"] as $item){
-            try{
-                if(!isset($this->getConfig()->getAll()["blocks"])) return;
-                $explode = explode("×", $item);
-                if(count($explode) === 1){
+        foreach ($this->getConfig()->getAll()["blocks"] as $item) {
+            try {
+                if (!isset($this->getConfig()->getAll()["blocks"])) return;
+                $explode = explode("=", $item);
+                if (count($explode) === 1) {
                     $b = ItemFactory::fromString((string)$item);
-                }elseif(count($explode) === 2){
+                } elseif (count($explode) === 2) {
                     $b = ItemFactory::fromString((string) $explode[0]);
                     $b = ItemFactory::fromString((string) $explode[1]);
                 }
-            }catch(\InvalidArgumentException $e){
+            }catch(\InvalidArgumentException $e) {
                 $this->getLogger()->error("Could not parse " . $item . " as a valid item, disable plugin...");
                 $this->getServer()->getPluginManager()->disablePlugin($this);
                 return;
             }
         }
-        $i = null;
-        try{
-            if(empty($this->getConfig()->get("blocks-replace", "minecraft:bedrock"))) return;
+        try {
+            if (empty($this->getConfig()->get("blocks-replace", "minecraft:bedrock"))) return;
             $i = ItemFactory::fromString((string)$this->getConfig()->get("blocks-replace", "minecraft:bedrock"));
-        }catch(\InvalidArgumentException $e){
-            $this->getLogger()->error("Could not parse " . $i . " as a valid item, disable plugin...");
+        }catch(\InvalidArgumentException $e) {
+            $this->getLogger()->error("Could not parse " . $this->getConfig()->get("blocks-replace") . " as a valid item, disable plugin...");
             $this->getServer()->getPluginManager()->disablePlugin($this);
             return;
         }
     }
-    
+
     /**
     * @param BlockBreakEvent $event
     * @return void
     */
-    public function onBlockBreak(BlockBreakEvent $event) : void{
+    public function onBlockBreak(BlockBreakEvent $event) : void {
         $block = $event->getBlock();
         $player = $event->getPlayer();
-        if($event->isCancelled()) return;
-        if(!$event->getBlock()->isCompatibleWithTool($event->getItem())) return;
-        if(!isset($this->getConfig()->getAll()["blocks"])) return;
-        if(empty($this->getConfig()->get("blocks-replace", "minecraft:bedrock"))) return;
+        if ($event->isCancelled()) return;
+        if (!$event->getBlock()->isCompatibleWithTool($event->getItem())) return;
+        if (!isset($this->getConfig()->getAll()["blocks"])) return;
+        if (empty($this->getConfig()->get("blocks-replace", "minecraft:bedrock"))) return;
         $blockReplace = ItemFactory::fromString((string)$this->getConfig()->get("blocks-replace", "minecraft:bedrock"));
         $replaceBlock = null;
-        $customReplace  = null;
-        foreach($this->getConfig()->getAll()["blocks"] as $value){
-            $explode = explode("×", $value);
-            if(count($explode) === 1){
+        $customReplace = null;
+        foreach ($this->getConfig()->getAll()["blocks"] as $value) {
+            $explode = explode("=", $value);
+            if (count($explode) === 1) {
                 $replaceBlock = ItemFactory::fromString((string) $value);
-            }elseif(count($explode) === 2){
+            } elseif (count($explode) === 2) {
                 $replaceBlock = ItemFactory::fromString((string) $explode[0]);
-                $customReplace  = ItemFactory::fromString((string) $explode[1]);
+                $customReplace = ItemFactory::fromString((string) $explode[1]);
             }
-            if($block->getId() === $replaceBlock->getId() and $block->getDamage() === $replaceBlock->getDamage()){
-                if(!$player->hasPermission("blockreplacer.bypass")) return;
-                if(!$block instanceof Solid) return;
-                if(in_array($block->getLevelNonNull()->getFolderName(), $this->getConfig()->getAll()["worlds"])) return;
-                foreach ($event->getDrops() as $drops){
-                    if((bool)$this->getConfig()->get("auto-pickup", true)){
-                        if(!$player->getInventory()->canAddItem($drops)) return;
-                        if(!$player->canPickupXp()) return;
+            if ($block->getId() === $replaceBlock->getId() and $block->getDamage() === $replaceBlock->getDamage()) {
+                if (!$player->hasPermission("blockreplacer.bypass")) return;
+                if (!$block instanceof Solid) return;
+                if (in_array($block->getLevelNonNull()->getFolderName(), $this->getConfig()->getAll()["worlds"])) return;
+                foreach ($event->getDrops() as $drops) {
+                    if ((bool)$this->getConfig()->get("auto-pickup", true)) {
+                        if (!$player->getInventory()->canAddItem($drops)) return;
+                        if (!$player->canPickupXp()) return;
                         $player->getInventory()->addItem($drops);
                         $player->addXp($event->getXpDropAmount());
                         continue;
@@ -114,13 +109,13 @@ class BlockReplacer extends PluginBase implements Listener{
                 }
                 $event->setCancelled();
                 $block->getLevelNonNull()->setBlock($block->asVector3(), BlockFactory::get($blockReplace->getId(), $blockReplace->getDamage()));
-                if(is_null($customReplace)){
+                if (is_null($customReplace)) {
                     $block->getLevelNonNull()->setBlock($block->asVector3(), BlockFactory::get($blockReplace->getId(), $blockReplace->getDamage()));
-                }else{
+                } else {
                     $block->getLevelNonNull()->setBlock($block->asVector3(), BlockFactory::get($customReplace ->getId(), $customReplace ->getDamage()));
                 }
                 $this->getScheduler()->scheduleDelayedTask(new ClosureTask(
-                    function(int $currentTick) use ($block) : void{
+                    function(int $currentTick) use ($block) : void {
                         $block->getLevelNonNull()->setBlock($block->asVector3(), BlockFactory::get($block->getId(), $block->getDamage()));
                     }
                 ), 20 * $this->getConfig()->get("cooldown", 60) ?? 60);
