85edef3e9fb19b58f0a228ac969aec2d45fbd647
AIPTU <anangp808@gmail.com>
2021-11-24 15:34:33 +0700

    Added ignore cancelled setting and fixed CS

--- src/aiptu/blockreplacer/BlockReplacer.php
+++ src/aiptu/blockreplacer/BlockReplacer.php
@@ -19,164 +19,164 @@ use function gettype;
 use function in_array;
 use function rename;
 
-class BlockReplacer extends PluginBase implements Listener
+final class BlockReplacer extends PluginBase implements Listener
 {
-    private const MODE_BLACKLIST = 0;
-    private const MODE_WHITELIST = 1;
+	private const MODE_BLACKLIST = 0;
+	private const MODE_WHITELIST = 1;
 
-    private int $mode;
+	private int $mode;
 
-    public function onEnable(): void
-    {
-        if (!class_exists(UpdateNotifier::class)) {
-            $this->getLogger()->error('UpdateNotifier virion not found. Download BlockReplacer at https://poggit.pmmp.io/p/BlockReplacer for a pre-compiled phar');
-            $this->getServer()->getPluginManager()->disablePlugin($this);
-            return;
-        }
+	public function onEnable(): void
+	{
+		if (!class_exists(UpdateNotifier::class)) {
+			$this->getLogger()->error('UpdateNotifier virion not found. Download BlockReplacer at https://poggit.pmmp.io/p/BlockReplacer for a pre-compiled phar');
+			$this->getServer()->getPluginManager()->disablePlugin($this);
+			return;
+		}
 
-        $this->checkConfig();
+		$this->checkConfig();
 
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 
-        UpdateNotifier::checkUpdate($this->getDescription()->getName(), $this->getDescription()->getVersion());
-    }
+		UpdateNotifier::checkUpdate($this->getDescription()->getName(), $this->getDescription()->getVersion());
+	}
 
-    public function onBlockBreak(BlockBreakEvent $event): void
-    {
-        if ($event->isCancelled()) {
-            return;
-        }
+	public function onBlockBreak(BlockBreakEvent $event): void
+	{
+		if (((bool) $this->getConfig()->get('ignore-cancelled', true)) && $event->isCancelled()) {
+			return;
+		}
 
-        $block = $event->getBlock();
-        $player = $event->getPlayer();
-        $world = $block->getLevelNonNull();
+		$block = $event->getBlock();
+		$player = $event->getPlayer();
+		$world = $block->getLevelNonNull();
 
-        if (!$block->isCompatibleWithTool($event->getItem())) {
-            return;
-        }
+		if (!$block->isCompatibleWithTool($event->getItem())) {
+			return;
+		}
 
-        if (!isset($this->getConfig()->getAll()['blocks'])) {
-            return;
-        }
+		if (!isset($this->getConfig()->getAll()['blocks'])) {
+			return;
+		}
 
-        $blockReplace = ItemFactory::fromString($this->getConfig()->get('blocks-replace', 'minecraft:bedrock'));
-        $replaceBlock = null;
-        $customReplace = null;
+		$blockReplace = ItemFactory::fromString($this->getConfig()->get('blocks-replace', 'minecraft:bedrock'));
+		$replaceBlock = null;
+		$customReplace = null;
 
-        foreach ($this->getConfig()->getAll()['blocks'] as $value) {
-            $explode = explode('=', $value);
+		foreach ($this->getConfig()->getAll()['blocks'] as $value) {
+			$explode = explode('=', $value);
 
-            if (count($explode) === 1) {
-                $replaceBlock = ItemFactory::fromString($value);
-            } elseif (count($explode) === 2) {
-                $replaceBlock = ItemFactory::fromString($explode[0]);
-                $customReplace = ItemFactory::fromString($explode[1]);
-            }
+			if (count($explode) === 1) {
+				$replaceBlock = ItemFactory::fromString($value);
+			} elseif (count($explode) === 2) {
+				$replaceBlock = ItemFactory::fromString($explode[0]);
+				$customReplace = ItemFactory::fromString($explode[1]);
+			}
 
-            if ($block->getId() === $replaceBlock->getId() && $block->getDamage() === $replaceBlock->getDamage()) {
-                if (!$this->checkWorlds($world)) {
-                    return;
-                }
+			if ($block->getId() === $replaceBlock->getId() && $block->getDamage() === $replaceBlock->getDamage()) {
+				if (!$this->checkWorlds($world)) {
+					return;
+				}
 
-                if (!$player->hasPermission('blockreplacer.bypass')) {
-                    return;
-                }
+				if (!$player->hasPermission('blockreplacer.bypass')) {
+					return;
+				}
 
-                foreach ($event->getDrops() as $drops) {
-                    if ((bool) $this->getConfig()->get('auto-pickup', true)) {
-                        (!$player->getInventory()->canAddItem($drops)) ? ($world->dropItem($block, $drops)) : ($player->getInventory()->addItem($drops));
-                        (!$player->canPickupXp()) ? ($world->dropExperience($block, $event->getXpDropAmount())) : ($player->addXp($event->getXpDropAmount()));
+				foreach ($event->getDrops() as $drops) {
+					if ((bool) $this->getConfig()->get('auto-pickup', true)) {
+						(!$player->getInventory()->canAddItem($drops)) ? ($world->dropItem($block, $drops)) : ($player->getInventory()->addItem($drops));
+						(!$player->canPickupXp()) ? ($world->dropExperience($block, $event->getXpDropAmount())) : ($player->addXp($event->getXpDropAmount()));
 
-                        continue;
-                    }
+						continue;
+					}
 
-                    $world->dropItem($block, $drops);
-                    $world->dropExperience($block, $event->getXpDropAmount());
-                }
+					$world->dropItem($block, $drops);
+					$world->dropExperience($block, $event->getXpDropAmount());
+				}
 
-                $event->setCancelled();
+				$event->setCancelled();
 
-                $world->setBlock($block, BlockFactory::get($blockReplace->getId(), $blockReplace->getDamage()));
+				$world->setBlock($block, BlockFactory::get($blockReplace->getId(), $blockReplace->getDamage()));
 
-                if ($customReplace === null) {
-                    $world->setBlock($block, BlockFactory::get($blockReplace->getId(), $blockReplace->getDamage()));
-                } else {
-                    $world->setBlock($block, BlockFactory::get($customReplace->getId(), $customReplace->getDamage()));
-                }
+				if ($customReplace === null) {
+					$world->setBlock($block, BlockFactory::get($blockReplace->getId(), $blockReplace->getDamage()));
+				} else {
+					$world->setBlock($block, BlockFactory::get($customReplace->getId(), $customReplace->getDamage()));
+				}
 
-                $this->getScheduler()->scheduleDelayedTask(new ClosureTask(
-                    function (int $currentTick) use ($block, $world): void {
-                        $world->setBlock($block, BlockFactory::get($block->getId(), $block->getDamage()));
-                    }
-                ), 20 * $this->getConfig()->get('cooldown', 60));
-            }
-        }
-    }
+				$this->getScheduler()->scheduleDelayedTask(new ClosureTask(
+					function (int $currentTick) use ($block, $world): void {
+						$world->setBlock($block, BlockFactory::get($block->getId(), $block->getDamage()));
+					}
+				), 20 * $this->getConfig()->get('cooldown', 60));
+			}
+		}
+	}
 
-    private function checkConfig(): void
-    {
-        $this->saveDefaultConfig();
+	private function checkConfig(): void
+	{
+		$this->saveDefaultConfig();
 
-        if ($this->getConfig()->get('config-version', 2) !== 2) {
-            $this->getLogger()->notice('Your configuration file is outdated, updating the config.yml...');
-            $this->getLogger()->notice('The old configuration file can be found at config.old.yml');
+		if ($this->getConfig()->get('config-version', 2) !== 2) {
+			$this->getLogger()->notice('Your configuration file is outdated, updating the config.yml...');
+			$this->getLogger()->notice('The old configuration file can be found at config.old.yml');
 
-            rename($this->getDataFolder() . 'config.yml', $this->getDataFolder() . 'config.old.yml');
+			rename($this->getDataFolder() . 'config.yml', $this->getDataFolder() . 'config.old.yml');
 
-            $this->reloadConfig();
-        }
+			$this->reloadConfig();
+		}
 
-        foreach ([
-            'cooldown' => 'integer',
-            'auto-pickup' => 'boolean',
-            'blocks' => 'array',
-            'blocks-replace' => 'string',
-            'mode' => 'string',
-            'worlds' => 'array',
-        ] as $option => $expectedType) {
-            if (($type = gettype($this->getConfig()->getNested($option))) !== $expectedType) {
-                throw new \TypeError("Config error: Option ({$option}) must be of type {$expectedType}, {$type} was given");
-            }
-        }
+		foreach ([
+			'cooldown' => 'integer',
+			'auto-pickup' => 'boolean',
+			'blocks' => 'array',
+			'blocks-replace' => 'string',
+			'mode' => 'string',
+			'worlds' => 'array',
+		] as $option => $expectedType) {
+			if (($type = gettype($this->getConfig()->getNested($option))) !== $expectedType) {
+				throw new \TypeError("Config error: Option ({$option}) must be of type {$expectedType}, {$type} was given");
+			}
+		}
 
-        match ($this->getConfig()->get('mode')) {
-            'blacklist' => $this->mode = self::MODE_BLACKLIST,
-            'whitelist' => $this->mode = self::MODE_WHITELIST,
-            default => throw new \InvalidArgumentException('Invalid mode selected, must be either "blacklist" or "whitelist"!'),
-        };
+		match ($this->getConfig()->get('mode')) {
+			'blacklist' => $this->mode = self::MODE_BLACKLIST,
+			'whitelist' => $this->mode = self::MODE_WHITELIST,
+			default => throw new \InvalidArgumentException('Invalid mode selected, must be either "blacklist" or "whitelist"!'),
+		};
 
-        if (!isset($this->getConfig()->getAll()['blocks'])) {
-            return;
-        }
+		if (!isset($this->getConfig()->getAll()['blocks'])) {
+			return;
+		}
 
-        foreach ($this->getConfig()->getAll()['blocks'] as $item) {
-            $explode = explode('=', $item);
+		foreach ($this->getConfig()->getAll()['blocks'] as $item) {
+			$explode = explode('=', $item);
 
-            try {
-                if (count($explode) === 1) {
-                    $replaceBlock = ItemFactory::fromString($item);
-                } elseif (count($explode) === 2) {
-                    $replaceBlock = ItemFactory::fromString($explode[0]);
-                    $customReplace = ItemFactory::fromString($explode[1]);
-                }
-            } catch (\InvalidArgumentException $e) {
-                throw $e;
-            }
-        }
+			try {
+				if (count($explode) === 1) {
+					$replaceBlock = ItemFactory::fromString($item);
+				} elseif (count($explode) === 2) {
+					$replaceBlock = ItemFactory::fromString($explode[0]);
+					$customReplace = ItemFactory::fromString($explode[1]);
+				}
+			} catch (\InvalidArgumentException $e) {
+				throw $e;
+			}
+		}
 
-        try {
-            $blockReplace = ItemFactory::fromString($this->getConfig()->get('blocks-replace', 'minecraft:bedrock'));
-        } catch (\InvalidArgumentException $e) {
-            throw $e;
-        }
-    }
+		try {
+			$blockReplace = ItemFactory::fromString($this->getConfig()->get('blocks-replace', 'minecraft:bedrock'));
+		} catch (\InvalidArgumentException $e) {
+			throw $e;
+		}
+	}
 
-    private function checkWorlds(Level $world): bool
-    {
-        if ($this->mode === self::MODE_BLACKLIST) {
-            return !(in_array($world->getFolderName(), $this->getConfig()->getAll()['worlds'], true));
-        }
+	private function checkWorlds(Level $world): bool
+	{
+		if ($this->mode === self::MODE_BLACKLIST) {
+			return !(in_array($world->getFolderName(), $this->getConfig()->getAll()['worlds'], true));
+		}
 
-        return in_array($world->getFolderName(), $this->getConfig()->getAll()['worlds'], true);
-    }
+		return in_array($world->getFolderName(), $this->getConfig()->getAll()['worlds'], true);
+	}
 }
